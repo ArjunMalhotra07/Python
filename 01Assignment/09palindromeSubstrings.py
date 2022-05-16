07a178be90c6789449d165f067781f86f5c37022
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2022-05-16 17:36:00 +0530

    Every Palindromic Substring in a Given Word

---
+++ 01Assignment/09palindromeSubstrings.py
@@ -0,0 +1,33 @@
+testString = "nitin"
+
+def everySubstring(testString):
+    length = len(testString)
+    allSubstrings = []
+    for j in range (1, length+1):
+        step = j
+        for i in range (0, length-1):
+            start = i
+            end = start + step + 1 
+            if end <= length:
+                allSubstrings.append(testString[start:end])
+    return(allSubstrings)
+    
+ansArray = everySubstring(testString)
+print("All Substrings : ",ansArray)
+
+def palindrome(ansArray):
+    newAnsArray = []
+    lengthArray = len(ansArray)
+    for i in range (0, lengthArray):
+        reversedString = ""
+        testWord = ansArray[i]
+        for j in range (len(testWord)-1, -1, -1):
+            reversedString += testWord[j]
+
+        if reversedString == testWord:
+            newAnsArray.append(reversedString)
+        
+        
+    print("Palindromic Substrings : ",newAnsArray)
+    
+palindrome(ansArray)
