a7c3e78ee560d65d1385c6d44b5f99b7108f1011
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2022-05-16 21:46:43 +0530

    Check if a String can be made Palindrome or not

--- 01Assignment/08dictionary.py
+++ 01Assignment/08dictionary.py
@@ -1,24 +1,40 @@
 inString = "arjun"
 
-dictionary = {}
-count = 1
+dictionaryMap = {}
+count = 0
 last = inString[len(inString)-1]
 
 for i in range (0, len(inString)-1):
-    for j in range (1, len(inString)):
-        if inString[i] in dictionary:
-            continue
-        else:
-            if inString[i] == inString[j]:
-                count += 1  
-    dictionary[inString[i]] = count
-    count = 0
+    if inString[i] not in dictionaryMap:
+        for j in range (i+1, len(inString)):
 
-if last not in dictionary:
-    dictionary[last] = 1
+                if inString[i] == inString[j]:
+                    count += 1  
+        dictionaryMap[inString[i]] = count + 1
+        count = 0
 
-print(dictionary)
+if last not in dictionaryMap:
+    dictionaryMap[last] = 1
 
+print(dictionaryMap)
+
+errCount = 0
+for key, val in dictionaryMap.items():
+    if val % 2 != 0:
+        errLetter = key
+        errCount += 1
+
+
+if errCount==1 :
+    print("String can be made Palindrome ")
+else:
+    print("String cannot be made Palindrome ")
+
+
+
+
+
+        
 
 
    
