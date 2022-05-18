9157e2f4dac54abf4a524caa7200fec678ce62ed
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2022-05-18 13:23:10 +0530

    Basic Strings

---
+++ 08Strings.py
@@ -0,0 +1,11 @@
+testString = "      Hello I am Arjun Malhotra"
+print(testString[:8])
+print(testString[-10:-5])
+print(testString[-10:])
+
+x = testString.split()
+print(x[2])
+print(testString.strip())
+
+print('.'.join(["F","B","I"]))
+print("am Ab" in testString)
